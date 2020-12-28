0c3fd61647e45663b99c3e70c2f6ebb39cd94ccc
ADAM <adamosen@hotmail.com>
2020-12-29 00:38:17 +0100

    ll

--- SocialMediaSida/js/cookies.js
+++ SocialMediaSida/js/cookies.js
@@ -3,7 +3,7 @@ function setCookie(cname,cvalue,exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    //alert( document.cookie );
+    alert( document.cookie );
   }
   
   function getCookie(cname) {
@@ -28,7 +28,7 @@ function setCookie(cname,cvalue,exdays) {
 
     var username = getCookie("username");
 
-    document.getElementById("alias").innerHTML = username;
+    document.getElementById("profileNavText").innerHTML = "Welcome "+ username;
   }
   
   function checkCookie() {
