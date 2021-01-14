db7c70f54387de219c52809296ab65d609428629
ADAM <adamosen@hotmail.com>
2021-01-14 21:29:33 +0100

    m

--- Portfolio/form-to-email.php
+++ Portfolio/form-to-email.php
@@ -21,12 +21,12 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'adamosen@hotmail.com';//<== update the email address
-$email_subject = "New Form submission";
+$email_from = 'adamosen@hotmail.com';//<== from me
+$email_subject = "New Form submission from $visitor_email ";
 $email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+    "Here is the message:\n $message \n".
     
-$to = "adamosen@hotmail.com";//<== update the email address
+$to = "adamosen@hotmail.com";//<== my mail
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
